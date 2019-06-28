f0ae693e5bb6c51ff466310e5f142b2415772870
orperel <or_perel@hotmail.com>
2019-06-28 11:34:19 +0300

    install scripts and updated requirements - removing older files

--- install_env.sh
+++
@@ -1,5 +0,0 @@
-conda create --name neural_modelling python=3.6 --file conda-spec-file.txt
-conda activate neural_modelling
-conda install -n neural_modelling pip
-conda install torchvision
-pip install -r requirements.txt
--- refresh_package_requirements.sh
+++
@@ -1,3 +0,0 @@
-# To be used by developers - whenever a new package dependency is added
-conda list --explicit > conda-spec-file.txt
-pip freeze > requirements.txt
