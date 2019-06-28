ea8929549e92143d9fc3291479c81f221a0130b7
orperel <or_perel@hotmail.com>
2019-06-28 11:44:28 +0300

    complete model and model loader

--- src/model/neural_model_net.py
+++ src/model/neural_model_net.py
@@ -1,7 +1,5 @@
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from model.modifiers_decoder import ModifiersDecoder
 
 
@@ -23,33 +21,51 @@ class NeuralModelNet(nn.Module):
         tensor = tensor * mask
         return tensor
 
-    def forward(self, imgs, modifiers, non_padded_modifiers_mask):
+    def encode_img_features(self, imgs):
         batch_dim, ref_imgs_dim = imgs.shape[:2]
         reshaped_imgs = imgs.reshape(-1, *imgs.shape[2:])
         x = self.feature_extractor_block(reshaped_imgs)
         x = x.reshape(batch_dim, ref_imgs_dim, x.shape[1])
 
-        # TODO: Fix comment here
-        # src_seq = BATCH x 3 x CNN_out_dim   (3 is for top/front/left, CNN_out_dim=512 or 2048 is ResNet feature vector)..
-        # src_pos = [1,2,3] (always, we use all 3 images, no masking..)
-        # tgt_seq = BATCH x N x dOUT.   (For each triplet, we have N modifiers, each represented with dOUT dimensions)
-        # tgt_pos = [1,2,3] (always, we use all 3 images, no masking..)
-        modifiers_cnt = modifiers.shape[1]
-        img_mask = torch.arange(1, ref_imgs_dim+1, device=x.device)
+        return x
+
+    def prepare_img2seq_enc_input(self, imgs, x):
+        batch_dim, ref_imgs_dim = imgs.shape[:2]
+        img_mask = torch.arange(1, ref_imgs_dim + 1, device=x.device)
         img_mask_per_ref_img = img_mask.repeat(batch_dim, 1)
+        src_seq = x
+        src_pos = img_mask_per_ref_img  # src_pos = tensor[1,2,3] (always, we use all ref images, no masking..)
+
+        return src_seq, src_pos
+
+    def prepare_img2seq_dec_input(self, imgs, x, modifiers):
+        batch_dim, ref_imgs_dim = imgs.shape[:2]
+        modifiers_cnt = modifiers.shape[1]
         modifiers_mask = torch.arange(1, modifiers_cnt+1, device=x.device)
         modifiers_mask = modifiers_mask.repeat(batch_dim, 1)
-        src_seq = x
-        src_pos = img_mask_per_ref_img
-        tgt_pos = modifiers_mask
-        tgt_seq = tgt_pos  # self.to_tgt_mask(modifiers_cnt)
-        tgt_emb = modifiers
+        tgt_pos = modifiers_mask    # tgt_pos = tensor[1,2,3, ..,  #Maximal Num of Modifiers in batch]
+        tgt_seq = tgt_pos           # Differs from traditional Transformer, we deal with embeddings, not "words"
+        tgt_emb = modifiers         # The actual decodings we feed
 
+        return tgt_pos, tgt_seq, tgt_emb
+
+    def forward(self, imgs, modifiers, non_padded_modifiers_mask):
+
+        # Extract features from images
+        x = self.encode_img_features(imgs)
+
+        # Convert input to format understandable by img2seq model (currently only Transformer is supported)
+        src_seq, src_pos = self.prepare_img2seq_enc_input(imgs, x)
+        tgt_pos, tgt_seq, tgt_emb = self.prepare_img2seq_dec_input(imgs, x, modifiers)
+
+        # Obtain encodings for modifier predictions
         modifier_encoding = self.img_to_seq_block(src_seq, src_pos, tgt_seq, tgt_pos, tgt_emb)
 
+        # Decode actual values from each modifier
         modifier_class_id, element_type_tensor, element_pos_tensor, modifier_params = \
             self.modifiers_decoder(modifier_encoding)
 
+        # Apply masks for stale modifier entries
         modifier_class_id = self._mask_rows(modifier_class_id, non_padded_modifiers_mask)
         element_type_tensor = self._mask_rows(element_type_tensor, non_padded_modifiers_mask)
         element_pos_tensor = self._mask_rows(element_pos_tensor, non_padded_modifiers_mask)
